b80b6e6cbc2a8eab677e9e9e1bdfd906f312afbd
Wajdwael <wajdabdelhai@gmail.com>
2024-10-26 18:08:20 +0300

    Initial Commit

---
+++ notesApp/notesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesAppApp.swift
+//  notesApp
+//
+//  Created by Wajd Wael on 23/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct notesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
